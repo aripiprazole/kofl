794d4dd9e3b7eaf59f6fb1f173278d298792e995
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-11-20 22:28:01 -0300

    add return

--- src/nativeMain/kotlin/stmt.kt
+++ src/nativeMain/kotlin/stmt.kt
@@ -1,9 +1,13 @@
+class Return(val value: KoflObject) : RuntimeException(null, null)
+
 sealed class Stmt {
   data class ExprStmt(val expr: Expr) : Stmt()
   data class Block(val decls: List<Stmt>) : Stmt()
 
   // TODO: replace List<Stmt> with Stmt.Block
   data class WhileStmt(val condition: Expr, val body: List<Stmt>) : Stmt()
+
+  data class ReturnStmt(val expr: Expr) : Stmt()
 
   // TODO: add type
   data class ValDecl(val name: Token, val value: Expr) : Stmt()
