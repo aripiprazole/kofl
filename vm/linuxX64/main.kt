62b79593e6b8659772767a0c2e5d5a0c2e565b28
Lorenzo Guimaraes <lorenzooguimaraes@gmail.com>
2020-11-22 21:04:50 -0300

    add binary ops

--- vm/linuxX64/main.kt
+++ vm/linuxX64/main.kt
@@ -1,7 +1,5 @@
 package com.lorenzoog.kofl.vm
 
-import kotlinx.cinterop.alloc
-import kotlinx.cinterop.allocArray
 import kotlinx.cinterop.memScoped
 
 // TODO: use a own heap
@@ -12,6 +10,11 @@ fun main(): Unit = memScoped {
   val chunk = Chunk()
   chunk.write(OpCode.OpConstant, 123)
   chunk.write(chunk.addConstant(1.2), 123)
+
+  chunk.write(OpCode.OpConstant, 123)
+  chunk.write(chunk.addConstant(13.2), 123)
+
+  chunk.write(OpCode.OpSum, 123)
   chunk.write(OpCode.OpReturn, 123)
 //  chunk.disassemble("test chunk")
 
