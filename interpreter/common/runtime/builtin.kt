da4e2c82e3544337b75767030e9a8c3c5c00ad4a
Lorenzo Guimaraes <lorenzooguimaraes@gmail.com>
2020-11-29 23:45:08 -0300

    add native functions

--- interpreter/common/runtime/builtin.kt
+++ interpreter/common/runtime/builtin.kt
@@ -1,2 +1,22 @@
 package com.lorenzoog.kofl.interpreter.runtime
 
+import com.lorenzoog.kofl.interpreter.backend.Descriptor
+import com.lorenzoog.kofl.interpreter.backend.Environment
+import com.lorenzoog.kofl.interpreter.backend.KoflObject
+import com.lorenzoog.kofl.interpreter.exceptions.KoflRuntimeException
+
+typealias KoflNativeCallable = (callSite: Descriptor, arguments: Map<String, KoflObject>, environment: Environment) -> Unit
+
+class NativeEnvironment {
+  private val functions = mapOf<String, KoflNativeCallable>(
+    "println" to { _, arguments, _ ->
+      println(arguments.entries.first().value.unwrap())
+    }
+  )
+
+  fun call(nativeCall: String, callSite: Descriptor, arguments: Map<String, KoflObject>, environment: Environment) {
+    val call = functions[nativeCall] ?: throw KoflRuntimeException.UndefinedFunction(nativeCall)
+
+    call(callSite, arguments, environment)
+  }
+}
