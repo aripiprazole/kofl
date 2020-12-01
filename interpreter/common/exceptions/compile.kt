9a3ee5ffcaaabaee2d4f8a25d278117c59df7413
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-11-30 21:41:15 -0300

    move type validator exceptions to exceptions/compile.kt

--- interpreter/common/exceptions/compile.kt
+++ interpreter/common/exceptions/compile.kt
@@ -5,5 +5,13 @@ import com.lorenzoog.kofl.frontend.KoflException
 sealed class KoflCompileTimeException(message: String) : KoflException("compile", message) {
   class UnresolvedVar(name: String) : KoflCompileTimeException("variable $name not found")
   class UnresolvedParameter(index: Int) : KoflCompileTimeException("unresolved parameter $index")
-  class InvalidType(expected: Any, current: Any) : KoflCompileTimeException("invalid $current expected $expected")
+  class MissingReturn : KoflCompileTimeException("missing return function body")
+
+  sealed class Type(message: String) : KoflException("static type", message) {
+    class NameNotFound(name: String) : Type("name $name not found!")
+    class TypeNotFound(name: String) : Type("type $name not found!")
+    class InvalidType(value: Any) : Type("invalid kofl type in $value")
+    class AlreadyResolvedVar(name: String) : Type("already resolved var $name")
+    class InvalidDeclaredType(current: Any, expected: Any) : Type("excepted $expected but got $current")
+  }
 }
