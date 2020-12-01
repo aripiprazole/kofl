5c55a50e3c98e6f4c5fccf890156af2e1d546618
Lorenzo Guimaraes <lorenzooguimaraes@gmail.com>
2020-12-01 15:43:55 -0300

    add error handler and stack trace printer

--- interpreter/common/exceptions/runtime.kt
+++ interpreter/common/exceptions/runtime.kt
@@ -2,12 +2,27 @@ package com.lorenzoog.kofl.interpreter.exceptions
 
 import com.lorenzoog.kofl.frontend.KoflException
 import com.lorenzoog.kofl.interpreter.backend.Descriptor
+import com.lorenzoog.kofl.interpreter.backend.Environment
 
-sealed class KoflRuntimeException(message: String) : KoflException("runtime", message) {
-  class AlreadyDeclaredVar(name: String) : KoflRuntimeException("var $name already declared")
-  class UndefinedVar(name: String) :  KoflRuntimeException("var $name does not exists")
-  class UndefinedFunction(name: String) :  KoflRuntimeException("function $name does not exists")
-  class ReassignImmutableVar(name: String) : KoflRuntimeException("var $name is immutable")
-  class MissingReturn(descriptor: Descriptor) : KoflRuntimeException("missing return in descriptor $descriptor")
-  class InvalidType(expected: Any, current: Any) : KoflRuntimeException("invalid $current expected $expected")
+sealed class KoflRuntimeException(
+  message: String,
+  val environment: Environment
+) : KoflException("runtime", message) {
+  class AlreadyDeclaredVar(name: String, callSite: Environment) :
+    KoflRuntimeException("var $name already declared", callSite)
+
+  class UndefinedVar(name: String, callSite: Environment) :
+    KoflRuntimeException("var $name does not exists", callSite)
+
+  class UndefinedFunction(name: String, callSite: Environment) :
+    KoflRuntimeException("function $name does not exists", callSite)
+
+  class ReassignImmutableVar(name: String, callSite: Environment) :
+    KoflRuntimeException("var $name is immutable", callSite)
+
+  class MissingReturn(descriptor: Descriptor, callSite: Environment) :
+    KoflRuntimeException("missing return in descriptor $descriptor", callSite)
+
+  class InvalidType(expected: Any, current: Any, callSite: Environment) :
+    KoflRuntimeException("invalid $current expected $expected", callSite)
 }
