1d03e50ff1e7637ab66a7e7a232642da72059f19
Lorenzo Guimaraes <lorenzooguimaraes@gmail.com>
2020-12-03 23:48:03 -0300

    rename builtin vars names

--- interpreter/common/runtime/builtin.kt
+++ interpreter/common/runtime/builtin.kt
@@ -4,14 +4,14 @@ import com.lorenzoog.kofl.interpreter.backend.Environment
 import com.lorenzoog.kofl.interpreter.typing.KoflType
 
 class Builtin internal constructor(environment: Environment) {
-  val String = environment.createClass(KoflType.String) {
+  val string = environment.createClass(KoflType.String) {
     constructor(mapOf("any" to KoflType.Any)) { _, arguments, _ ->
       val any by arguments
 
       any.map { it.toString() }
     }
   }
-  val Int = environment.createClass(KoflType.Int)
-  val Double = environment.createClass(KoflType.Double)
-  val Unit = environment.createSingleton(KoflType.Unit)
+  val int = environment.createClass(KoflType.Int)
+  val double = environment.createClass(KoflType.Double)
+  val unit = environment.createSingleton(KoflType.Unit)
 }
