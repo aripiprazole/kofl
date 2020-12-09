9849dc87457d565901a5599e39de1942eee5eac8
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-12-09 03:51:04 -0300

    add C project to runtime library be faster, and be rather free than using kotlin native

--- interpreter/linuxX64/main.kt
+++ interpreter/linuxX64/main.kt
@@ -1,8 +1,14 @@
 package com.lorenzoog.kofl.interpreter
 
+import kotlinx.cinterop.cstr
+import kotlinx.cinterop.toKString
+import sample.globalstate.doSomething
+
 private val consoleSender: ConsoleSender = ErrorHandlerImpl()
 
 fun main(args: Array<String>) {
+  println(doSomething("Hello, kotlin native interop".cstr)?.toKString())
+
   try {
     startRepl(consoleSender,
       debug = args.contains("-debug"),
