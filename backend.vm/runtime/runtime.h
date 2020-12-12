dccb4cef6cfbc7b7aea30747110ce6806f88bc00
Lorenzo Guimaraes <lorenzooguimaraes@gmail.com>
2020-12-11 23:40:41 -0300

    migrate some vm code to C

--- backend.vm/runtime/runtime.h
+++
@@ -1,8 +0,0 @@
-#ifndef RUNTIME_H
-#define RUNTIME_H
-
-#include<stdlib.h>
-
-typedef void callable(void *);
-
-#endif //RUNTIME_H
--- backend.vm/runtime/value.h
+++ backend.vm/runtime/value.h
@@ -1,10 +1,22 @@
 #ifndef RUNTIME_VALUE_H
 #define RUNTIME_VALUE_H
 
+#include "heap.h"
+
 typedef union value {
     int int_;
     double double_;
     char *string_;
 } value_t;
+
+typedef struct value_array {
+    int count;
+    int capacity;
+    value_t *values;
+} value_array_t;
+
+value_array_t* value_array_create(int count, int capacity);
+
+void value_array_write(value_t value);
 
 #endif //RUNTIME_VALUE_H
