2a917a4b76f2d68fc888d29cc91c06c49fb00ab7
Lorenzo Guimaraes <lorenzooguimaraes@gmail.com>
2020-12-13 02:46:48 -0300

    migrate compiler to descriptor visitor

--- backend.vm/native/Main.kt
+++ backend.vm/native/Main.kt
@@ -1,16 +1,30 @@
 package com.lorenzoog.kofl.vm
 
+import com.lorenzoog.kofl.compiler.kvm.backend.Compiler
+import com.lorenzoog.kofl.compiler.kvm.backend.Descriptor
+import com.lorenzoog.kofl.compiler.kvm.typing.KoflType
+import com.lorenzoog.kofl.compiler.kvm.typing.TypeContainer
 import com.lorenzoog.kofl.frontend.Parser
 import com.lorenzoog.kofl.frontend.Scanner
-import com.lorenzoog.kofl.interpreter.internal.chunk_dump
-import com.lorenzoog.kofl.vm.interop.OpCode
+import com.lorenzoog.kofl.frontend.Stack
+import com.lorenzoog.kofl.vm.compiler.BytecodeCompiler
 import kotlinx.cinterop.memScoped
-import kotlinx.cinterop.pointed
-import kotlinx.cinterop.toKString
+
+const val MAX_STACK = 250
+
+val builtinTypeContainer = TypeContainer().apply {
+  defineType("Any", KoflType.Any)
+  defineType("String", KoflType.String)
+  defineType("Int", KoflType.Int)
+  defineType("Double", KoflType.Double)
+  defineType("Boolean", KoflType.Boolean)
+  defineType("Unit", KoflType.Unit)
+}
 
 fun main(): Unit = memScoped {
-  val compiler = BytecodeCompiler()
+  val locals = mutableMapOf<Descriptor, Int>()
 
+  val compiler = BytecodeCompiler()
   val lexer = Scanner(
     """
       val x: String = "";
@@ -19,16 +33,20 @@ fun main(): Unit = memScoped {
     """.trimIndent()
   )
   val parser = Parser(lexer.scan(), repl = true)
+  val descriptorCompiler = Compiler(locals, Stack<TypeContainer>(MAX_STACK).also { container ->
+    container.push(builtinTypeContainer.copy())
+  })
+
   val stmts = parser.parse()
+  val descriptors = descriptorCompiler.compile(stmts)
 
   println("== PARSED ==")
-  println(stmts)
+  println("STMTS = $stmts")
+  println("DESCRIPTORS = $descriptors")
   println("== ------ ==")
 
   println("== COMPILED ==")
-  compiler.compile(stmts).forEachIndexed { i, chunk ->
-    chunk.pointed.disassemble("CODE $i")
-  }
+  compiler.compile(descriptors).disassemble("CODE")
   println("== -------- ==")
 }
 
---
+++ backend.vm/native/compiler/BytecodeCompiler.kt
@@ -0,0 +1,168 @@
+@file:OptIn(ExperimentalUnsignedTypes::class)
+
+package com.lorenzoog.kofl.vm.compiler
+
+import com.lorenzoog.kofl.compiler.kvm.backend.*
+import com.lorenzoog.kofl.compiler.kvm.typing.KoflType
+import com.lorenzoog.kofl.vm.interop.*
+import kotlinx.cinterop.CValue
+import kotlinx.cinterop.MemScope
+import kotlinx.cinterop.cstr
+import kotlinx.cinterop.placeTo
+import platform.posix.UINT8_MAX
+
+val Descriptor.line
+  get() = -1
+
+class BytecodeCompiler : Descriptor.Visitor<Unit> {
+  private val heap = MemScope()
+  private val chunk = Chunk()
+
+  fun compile(descriptors: Collection<Descriptor>): Chunk {
+    visitDescriptors(descriptors)
+    emit(OpCode.OP_RET, -1)
+
+    return chunk
+  }
+
+  override fun visitConstDescriptor(descriptor: ConstDescriptor) {
+    val value = descriptor.value.toString()
+    val const = when (descriptor.type) {
+      KoflType.String -> makeConst(value)
+      KoflType.Boolean -> makeConst(value.toBoolean())
+      KoflType.Double -> makeConst(value.toDouble())
+      KoflType.Int -> makeConst(value.toInt())
+      else -> error("UNSUPPORTED CONST TYPE ${descriptor.type} (${descriptor.value::class})")
+    }
+    emit(OpCode.OP_CONST, const, descriptor.line)
+  }
+
+  override fun visitThisDescriptor(descriptor: ThisDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitSetDescriptor(descriptor: SetDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitGetDescriptor(descriptor: GetDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitCallDescriptor(descriptor: CallDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitAccessVarDescriptor(descriptor: AccessVarDescriptor) {
+    emit(OpCode.OP_CONST, makeConst(descriptor.name), descriptor.line)
+    emit(OpCode.OP_ACCESS_GLOBAL, descriptor.line)
+  }
+
+  override fun visitAccessFunctionDescriptor(descriptor: AccessFunctionDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitUnaryDescriptor(descriptor: UnaryDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitValDescriptor(descriptor: ValDescriptor) {
+    emit(OpCode.OP_CONST, makeConst(descriptor.name), descriptor.line)
+    visitDescriptor(descriptor.value)
+    emit(OpCode.OP_STORE_GLOBAL, descriptor.line)
+  }
+
+  override fun visitVarDescriptor(descriptor: VarDescriptor) {
+    emit(OpCode.OP_CONST, makeConst(descriptor.name), descriptor.line)
+    visitDescriptor(descriptor.value)
+    emit(OpCode.OP_STORE_GLOBAL, descriptor.line)
+  }
+
+  override fun visitAssignDescriptor(descriptor: AssignDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitReturnDescriptor(descriptor: ReturnDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitBlockDescriptor(descriptor: BlockDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitWhileDescriptor(descriptor: WhileDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitIfDescriptor(descriptor: IfDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitLogicalDescriptor(descriptor: LogicalDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitBinaryDescriptor(descriptor: BinaryDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitLocalFunctionDescriptor(descriptor: LocalFunctionDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitNativeFunctionDescriptor(descriptor: NativeFunctionDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitFunctionDescriptor(descriptor: FunctionDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  override fun visitClassDescriptor(descriptor: ClassDescriptor) {
+    TODO("Not yet implemented")
+  }
+
+  private fun makeConst(value: Boolean) = makeConst(Value(ValueType.V_TYPE_BOOL) {
+    _bool = value
+  })
+
+  private fun makeConst(value: Double) = makeConst(Value(ValueType.V_TYPE_DOUBLE) {
+    _double = value
+  })
+
+  private fun makeConst(value: Int) = makeConst(Value(ValueType.V_TYPE_INT) {
+    _int = value
+  })
+
+  private fun makeConst(value: String) = makeConst(Value(ValueType.V_TYPE_STR) {
+    _string = value.cstr.placeTo(heap)
+  })
+
+  @OptIn(ExperimentalUnsignedTypes::class)
+  private fun makeConst(value: CValue<Value>): UInt {
+    val const = chunk.addConst(value)
+
+    if (const > UINT8_MAX) {
+      error("TOO LONG CONST") // TODO: make a error
+    }
+
+    return const.toUInt()
+  }
+
+  private fun endCompiler() {
+    emit(OpCode.OP_RET, -1)
+  }
+
+  private fun emit(op: OpCode, line: Int) {
+    emit(op.value, line)
+  }
+
+  private fun emit(op: UInt, line: Int) {
+    chunk.write(op, line)
+  }
+
+  private fun emit(op: OpCode, value: UInt, line: Int) {
+    emit(op, line)
+    emit(value, line)
+  }
+}
