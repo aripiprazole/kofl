04753c4fd691c8f897c1da8222b0b73aa6125c1f
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2020-12-29 06:35:25 -0300

    fix grouping

--- frontend/common/parser/grammar/Token.kt
+++ frontend/common/parser/grammar/Token.kt
@@ -7,35 +7,45 @@ import com.lorenzoog.kofl.frontend.TokenType
 import com.lorenzoog.kofl.frontend.parser.lib.*
 
 internal object Token {
-  val Expression: Parser<Expr> = Math.rule
+  val token = lexeme(regex("""\s*""".toRegex()))
 
-  val Spaces = regex("""\s*""".toRegex())
-  val Token = lexeme(Spaces)
+  val Expression: Parser<Expr> = Math.rule
 
   val Plus = text(TokenType.Plus, "+")
   val Minus = text(TokenType.Minus, "-")
   val Star = text(TokenType.Star, "*")
   val Slash = text(TokenType.Slash, "/")
 
-  val String = regex(TokenType.String, """^"[a-zA-Z0-9]*"$""".toRegex())
-  val Identifier = regex(TokenType.Identifier, """^[a-zA-Z][a-zA-Z0-9]*$""".toRegex())
-
   val LeftParen = text(TokenType.LeftParen, "(")
   val RightParen = text(TokenType.RightParen, ")")
 
-  val Decimal = label("decimal")(regex("""\d+(?:\.\d+)?""".toRegex())) map {
+  val Decimal = label("decimal")(predicate(Matchers.number) map {
     it.toDouble()
-  }
+  })
 
-  val Const = Token(Decimal) map { Expr.Literal(it, line) }
+  val Numeric = token(Decimal).mapToLiteral()
 
-  val Group = combine(Token(LeftParen), Token(Expression), Token(RightParen)) { _, value, _ ->
-    Expr.Grouping(value, line)
-  }
+  val Text = label("text")(
+    token(regex(TokenType.String, """^"[a-zA-Z0-9]*"$""".toRegex())).mapToLiteral()
+  )
 
-  val Primary = Group or Const
+  val Identifier = label("identifier")(
+    token(regex(TokenType.Identifier, """[a-zA-Z][a-zA-Z0-9_]*""".toRegex())) map {
+      Expr.Var(it, line)
+    }
+  )
+
+  val Group = label("group")(
+    combine(LeftParen, token(Expression), RightParen) { _, value, _ ->
+      Expr.Grouping(value, line)
+    }
+  )
+
+  val Primary = label("primary")(
+    Identifier or Text or Numeric or Group
+  )
 
   operator fun <T> invoke(parser: Parser<T>): Parser<T> {
-    return Token(parser)
+    return token(parser)
   }
 }
