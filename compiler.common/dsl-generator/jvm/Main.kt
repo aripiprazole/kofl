7bb1b693f87665a8a5dade220735c4edc0b36867
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-01-01 18:47:22 -0300

    fix not generating generics BUILDING NOW!

--- compiler.common/dsl-generator/jvm/Main.kt
+++ compiler.common/dsl-generator/jvm/Main.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.util.Disposer
 import com.intellij.psi.PsiManager
 import com.intellij.testFramework.LightVirtualFile
 import com.squareup.kotlinpoet.*
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
 import org.jetbrains.kotlin.cli.common.messages.MessageRenderer
 import org.jetbrains.kotlin.cli.common.messages.PrintingMessageCollector
@@ -13,6 +14,7 @@ import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.idea.KotlinFileType
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtImportList
 import org.jetbrains.kotlin.psi.psiUtil.isAbstract
 import java.io.File
 
@@ -45,6 +47,17 @@ fun main() {
     LightVirtualFile("temp.kt", KotlinFileType.INSTANCE, file.readText())
   ) as KtFile? ?: error("File is null")
 
+  val imports = ktFile.children.filterIsInstance<KtImportList>()
+    .flatMap { it.imports }
+    .map { it.importPath.toString() }
+
+  fun findImportPackage(name: String): String {
+    return when (val packageName = imports.find { it.split(".").lastOrNull() == name }) {
+      is String -> packageName.split(".").let { it.take(it.size - 1).joinToString(".") }
+      else -> currentPackage
+    }
+  }
+
   val descriptors = buildMap<String, TypeSpec> {
     ktFile.children
       .filterIsInstance<KtClass>()
@@ -59,11 +72,42 @@ fun main() {
           primaryConstructor(FunSpec.constructorBuilder().apply {
             constructor.valueParameters.forEach parameterIter@{ parameter ->
               val parameterName = parameter.name ?: error("$parameter should have a name!")
-              val (_, typeName) = parameter.text.replace(" ", "").split(":")
 
-              if(typeName.contains("<") || typeName.contains(">")) return@parameterIter
+              addParameter(parameterName, parameter.text
+                .also { println("TEXT: $it") }
+                .split(":").getOrNull(1).let { it ?: error("parameter $parameterName should have a type!") }
+                .let mapping@{ typeName ->
+                  fun findTypeRecursive(name: String, recursive: Boolean = false): TypeName {
+                    val realName = if ("<" in name) {
+                      name.substringBefore("<")
+                    } else {
+                      name
+                    }
 
-              addParameter(parameterName, ClassName("", typeName))
+                    println("RECURSIVE: $recursive")
+                    println("NAME: $name")
+                    println("REAL NAME: $realName")
+
+                    val type = ClassName(findImportPackage(realName), realName)
+
+                    if ("<" in name) {
+                      val parameters = name.substringAfter("<").substringBefore(">").split(",").map { it.trim() }
+
+                      println("PARAMETERS: $parameters")
+                      println()
+
+                      return type.parameterizedBy(parameters.map {
+                        findTypeRecursive(it, true)
+                      })
+                    }
+
+                    println()
+
+                    return type
+                  }
+
+                  findTypeRecursive(typeName.trim())
+                })
             }
           }.build())
         }.build())
