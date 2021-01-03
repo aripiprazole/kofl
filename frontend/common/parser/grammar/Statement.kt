7d3f3b4727bf9a4ec170d6c44f577ee63a4f02fe
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-01-03 19:10:54 -0300

    add logical expressions

--- frontend/common/parser/grammar/Statement.kt
+++ frontend/common/parser/grammar/Statement.kt
@@ -5,13 +5,13 @@ import com.lorenzoog.kofl.frontend.parser.lib.*
 
 internal object Statement : Grammar<Stmt>() {
   private val ExprStmt = label("expr-stmt")(
-    combine(Expression, Semicolon) { stmt, _ ->
+    combine(Logical, Semicolon) { stmt, _ ->
       Stmt.ExprStmt(stmt, line)
     }
   )
 
   private val ReturnStmt = label("return-stmt")(
-    combine(Keywords.Return, Expression, Semicolon) { _, value, _ ->
+    combine(Keywords.Return, Logical, Semicolon) { _, value, _ ->
       Stmt.ReturnStmt(value, line)
     }
   )
--- frontend/jvm/Test.kt
+++ frontend/jvm/Test.kt
@@ -5,7 +5,7 @@ import com.lorenzoog.kofl.frontend.parser.lib.unwrapOr
  * TODO: remove me
  */
 fun main() {
-  val input = """func main(): Int { return 0; }""".trimIndent()
+  val input = """func main(): Int { return 0 && 5; }""".trimIndent()
 
   println(Func.parse(input).unwrapOr { result ->
     println("Expected: ${result.expected}; Actual: ${result.actual}; Char: ${input.getOrNull(result.actual.index)}")
