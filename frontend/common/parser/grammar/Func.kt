3c52a8c97f7e6f3042adb1de82abf57124974b41
Lorenzo Guimaraes <lorenzooguimaraes@gmail.com>
2021-01-04 09:54:11 -0300

    fix function not working without types in expression body

--- frontend/common/parser/grammar/Func.kt
+++ frontend/common/parser/grammar/Func.kt
@@ -2,68 +2,61 @@ package com.lorenzoog.kofl.frontend.parser.grammar
 
 import com.lorenzoog.kofl.frontend.Expr
 import com.lorenzoog.kofl.frontend.Stmt
+import com.lorenzoog.kofl.frontend.Token
 import com.lorenzoog.kofl.frontend.parser.lib.*
 
 internal object Func : Grammar<Expr>() {
-  private val ReturnType = label("return-type")(
-    combine(Colon, Identifier) { _, (name) ->
-      name
-    }
-  )
+  private fun handleParameters(parameterList: List<Pair<Expr.Var, Expr.Var>>): Map<Token, Token> {
+    return parameterList
+      .groupBy(
+        keySelector = { it.first.name },
+        valueTransform = { it.second.name }
+      )
+      .mapValues { it.value.first() }
+  }
 
-  private val Parameter = label("parameter")(
-    combine(Identifier, Colon, Identifier) { name, _, type ->
-      name to type
-    }
-  )
+  private val ReturnType = label("return-type")(combine(Colon, Identifier) { _, (name) -> name })
+  private val Parameter = label("parameter")(combine(Identifier, Colon, Identifier) { name, _, type -> name to type })
 
   private val Parameters = label("parameters")(
-    combine(LeftParen, many(Parameter), RightParen) { _, parameters, _ ->
-      parameters
-    }
+    combine(LeftParen, many(Parameter), RightParen) { _, parameters, _ -> parameters }
   )
 
   private val ExpressionBody = label("expression-body")(
-    combine(Equal, token(this)) { _, expr ->
-      listOf(Stmt.ExprStmt(expr, line))
-    }
+    combine(Equal, token(this)) { _, expr -> listOf(Stmt.ReturnStmt(expr, line)) }
   )
 
   private val Body = label("body")(
-    ExpressionBody or combine(LeftBrace, many(Statement), RightBrace) { _, body, _ ->
-      body
-    }
+    ExpressionBody or combine(LeftBrace, many(Statement), RightBrace) { _, body, _ -> body }
   )
 
-  private val NativeFunc = label("common-fun")(
+  private val NativeFunc = label("native-fun")(
     combine(
       Keywords.External, Keywords.Func, Identifier, Parameters, ReturnType.optional()
     ) { _, _, (name), parameterList, returnType ->
-      val parameters = parameterList
-        .groupBy(
-          keySelector = { it.first.name },
-          valueTransform = { it.second.name }
-        )
-        .mapValues { it.value.first() }
-
-      Expr.NativeFunc(name, parameters, returnType, line)
+      Expr.NativeFunc(name, handleParameters(parameterList), returnType, line)
     }
   )
 
-  private val CommonFunc = label("common-func")(
-    combine(
-      Keywords.Func, Identifier, Parameters, ReturnType.optional(), Body
-    ) { _, (name), parameterList, returnType, body ->
-      val parameters = parameterList
-        .groupBy(
-          keySelector = { it.first.name },
-          valueTransform = { it.second.name }
-        )
-        .mapValues { it.value.first() }
-
-      Expr.CommonFunc(name, parameters, body, returnType, line)
+  private val UntypedCommonFunc = label("untyped-common-func")(
+    combine(Keywords.Func, Identifier, Parameters, nullable<Token>(), Body) { _, (name), parameterList, _, body ->
+      Expr.CommonFunc(name, handleParameters(parameterList), body, null, line)
     }
   )
 
-  override val rule: Parser<Expr> = NativeFunc or CommonFunc or Logical
+  private val TypedCommonFunc = label("common-func")(
+    UntypedCommonFunc or
+      combine(Keywords.Func, Identifier, Parameters, ReturnType, Body) { _, (name), parameterList, returnType, body ->
+        val parameters = parameterList
+          .groupBy(
+            keySelector = { it.first.name },
+            valueTransform = { it.second.name }
+          )
+          .mapValues { it.value.first() }
+
+        Expr.CommonFunc(name, parameters, body, returnType, line)
+      }
+  )
+
+  override val rule: Parser<Expr> = NativeFunc or TypedCommonFunc or Logical
 }
